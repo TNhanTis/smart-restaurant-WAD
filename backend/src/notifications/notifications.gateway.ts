b41ba32ef4456ab676da627c7ba04cb786ed1fa2
23120040 <anhminsen111@gmail.com>
2026-01-12 21:27:31 +0700

    Init decorator for WebSocket server

---
+++ backend/src/notifications/notifications.gateway.ts
@@ -0,0 +1,21 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  ConnectedSocket,
+  MessageBody,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { Logger, UseGuards } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@WebSocketGateway({
+  cors: {
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true,
+  },
+  namespace: '/notifications',
+})
+export class NotificationsGateway {}
