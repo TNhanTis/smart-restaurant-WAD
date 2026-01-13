6ff572bde7f313befb0d1473ab0430b47906c933
23120040 <anhminsen111@gmail.com>
2026-01-13 22:49:24 +0700

    feat(payment): tạo cấu trúc cho m

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import { MomoService } from './momo/momo.service';
+import { ZalopayService } from './zalopay/zalopay.service';
+import { VnpayService } from './vnpay/vnpay.service';
+import { CashService } from './cash/cash.service';
 
 @Module({
   controllers: [PaymentsController],
-  providers: [PaymentsService]
+  providers: [PaymentsService, MomoService, ZalopayService, VnpayService, CashService]
 })
 export class PaymentsModule {}
