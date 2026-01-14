d834ff8e5b6d86f05d0040a6aee08f155b44a631
23120040 <anhminsen111@gmail.com>
2026-01-14 11:13:39 +0700

    Cash service

--- backend/src/payments/payments.service.ts
+++ backend/src/payments/payments.service.ts
@@ -5,7 +5,7 @@ import {
 } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { MomoService } from './momo/momo.service';
-import { ZalopayService } from './zalopay/zalopay.service';
+import { ZaloPayService } from './zalopay/zalopay.service';
 import { VnpayService } from './vnpay/vnpay.service';
 import { CashService } from './cash/cash.service';
 
@@ -14,7 +14,7 @@ export class PaymentsService {
   constructor(
     private readonly prisma: PrismaService,
     private readonly momoService: MomoService,
-    private readonly zalopayService: ZalopayService,
+    private readonly zaloPayService: ZaloPayService,
     private readonly vnpayService: VnpayService,
     private readonly cashService: CashService,
   ) {}
