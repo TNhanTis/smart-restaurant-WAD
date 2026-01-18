2db06952dea185f11d4e64dbe0fdd8653a2c45d1
23120040 <anhminsen111@gmail.com>
2026-01-18 18:03:15 +0700

    feat(waiter): Implement Socket.IO event emissions for order lifecycle

--- backend/src/waiter/waiter.module.ts
+++ backend/src/waiter/waiter.module.ts
@@ -3,9 +3,10 @@ import { WaiterController } from './waiter.controller';
 import { WaiterService } from './waiter.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { TablesModule } from '../tables/tables.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [PrismaModule, TablesModule],
+  imports: [PrismaModule, TablesModule, NotificationsModule],
   controllers: [WaiterController],
   providers: [WaiterService],
   exports: [WaiterService],
